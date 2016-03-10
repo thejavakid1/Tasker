ec530819a39e5e6ec804ced0ec378d7078f0958e
thejavakid1 <thejavakid1@gmail.com>
2016-03-09 20:39:52 -0800

    changed min sdk to 16 and added in notification code.

--- src/main/java/com/Main/Tasker/ScriptStart.java
+++ src/main/java/com/Main/Tasker/ScriptStart.java
@@ -1,8 +1,15 @@
 package com.Main.Tasker;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
+import android.renderscript.Allocation;
+import android.support.design.widget.Snackbar;
+
+import java.io.IOException;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -18,6 +25,17 @@ public class ScriptStart extends IntentService{
     }
      @Override
      protected void onHandleIntent(Intent workIntent) {
+         try {
+             Runtime.getRuntime().exec("su -c echo");
+         } catch (IOException e) {
+             PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), workIntent, 0);
+             Notification n  = new Notification.Builder(this)
+                     .setContentTitle("Tasker:")
+                     .setContentText("Starting Scripts Failed!").build();
+             NotificationManager notificationManager =(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+             notificationManager.notify(0, n);
+         }
          String dataString = workIntent.getDataString();
      }
 }
